ae516dc82545f540e699d8c41fc302060c4a4d80
李庆阳 <>
2018-06-12 19:46:37 +0800

    add drawPath

--- index.js
+++ index.js
@@ -228,6 +228,7 @@ export default function (d3) {
             dispatch.on(typename, callback);
             return brush;
         };
+        brush.drawPath = drawPath
 
         return brush;
     };
